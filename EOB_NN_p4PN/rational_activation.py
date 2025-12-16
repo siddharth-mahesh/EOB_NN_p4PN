9629af01b777596d0d5d3ae350264d081883ac02
siddharth-mahesh <sm0193@mix.wvu.edu>
2025-12-16 02:55:50 -0500

    added rational activations and neural networks

---
+++ EOB_NN_p4PN/rational_activation.py
@@ -0,0 +1,55 @@
+import jax
+import jax.numpy as jnp
+import equinox as eqx
+
+class RationalActivation(eqx.Module):
+    """
+    A rational activation function with learnable polynomial coefficients.
+    
+                          1  +   p_1 x + p_2 x^2 + ... + p_n x^n
+    Implements: f(x) = -----------------------------------------
+                          1  +   q_1 x + q_2 x^2 + ... + q_m x^m
+    
+    Parameters:
+        p: Scale factors (shape: (features, degrees of p - 1))
+        q: Frequency/Steepness factors (shape: (features, degrees of q - 1))
+    """
+    p: jax.Array
+    q: jax.Array
+
+    def __init__(self, key: jax.Array, features: int, degree_of_p: int, degree_of_q: int):
+        """
+        Args:
+            key: PRNG key for initialization.
+            features: The number of neurons (input dimension).
+            degree_of_p: The degree of the numerator polynomial.
+            degree_of_q: The degree of the denominator polynomial.
+        """
+        k_p, k_q = jax.random.split(key, 2)
+        self.p = jax.random.normal(k_p, (features,degree_of_p - 1)) * 0.1
+        self.q = jax.random.normal(k_q, (features,degree_of_q - 1)) * 0.1
+
+    def __call__(self, x: jax.Array) -> jax.Array:
+        """
+        Applies the activation function element-wise.
+        Expected input shape: (..., features)
+        """
+        x_expanded = x[..., None]
+        
+        x_powers_p = jnp.power(x_expanded, jnp.arange(1, self.p.shape[-1] + 1))
+        numerator = 1.0 + jnp.einsum('...fd, fd -> ...f', x_powers_p, self.p)
+        x_powers_q = jnp.power(x_expanded, jnp.arange(1, self.q.shape[-1] + 1))
+        denominator = 1.0 + jnp.einsum('...fd, fd -> ...f', x_powers_q, self.q)
+        return numerator / denominator
+
+
+if __name__ == "__main__":
+    key = jax.random.PRNGKey(42)
+    model_key, data_key = jax.random.split(key)
+    activation = RationalActivation(model_key, features=5, degree_of_p=4, degree_of_q=5)
+    x_input = jax.random.normal(data_key, (5,))
+    output = activation(x_input)
+    def loss_fn(model, x):
+        pred = model(x)
+        return jnp.sum(pred ** 2)
+    grads = eqx.filter_grad(loss_fn)(activation, x_input)
---
+++ EOB_NN_p4PN/rational_net.py
@@ -0,0 +1,50 @@
+import jax
+import jax.numpy as jnp
+
+# set jax to 64 bit precision
+jax.config.update("jax_enable_x64", True)
+import equinox as eqx
+from EOB_NN_p4PN.rational_activation import RationalActivation
+
+class RationalNet(eqx.Module):
+    extender: eqx.nn.Linear
+    activation: RationalActivation
+    scalarizer: eqx.nn.Linear
+    """
+    A Rational Neural Network model.
+    This model serves as a building block 
+    for the PN-mimicker neural networks built in this repo. 
+    It is a Linear transformation that extends the input dimension
+    followed by a rational activation function
+    followed by another linear transformation that returns a scalar.
+    """
+
+    def __init__(self, key, input_dim, hidden_dim, degree_of_p, degree_of_q):
+        """
+        Initialize the RationalNet.
+
+        Args:
+        key (jax.random.PRNGKey): The random key for initialization.
+        input_dim (int): The dimension of the input.
+        hidden_dim (int): The dimension of the hidden layer.
+        degree_of_p (int): The degree of the numerator polynomial.
+        degree_of_q (int): The degree of the denominator polynomial.
+        """
+        key1, key2, key3 = jax.random.split(key, 3)
+        self.extender = eqx.nn.Linear(input_dim, hidden_dim, key=key1)
+        self.activation = RationalActivation(key=key2, features=hidden_dim, degree_of_p=degree_of_p, degree_of_q=degree_of_q)
+        self.scalarizer = eqx.nn.Linear(hidden_dim, "scalar", key=key3)
+
+    def _single_forward(self, x):
+        """
+        Compute the forward pass for a single input.
+        """
+        h = self.extender(x)
+        h = self.activation(h)
+        return self.scalarizer(h)
+
+    def __call__(self, x):
+        """
+        Compute the forward pass for a batch of inputs.
+        """
+        return jax.vmap(self._single_forward, in_axes=0)(x)
